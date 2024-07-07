b7385d11e004584acf4e5ea8e6068ad0a41dec9a
2Boys <iSkiddedBoys@gmail.com>
2024-07-07 11:47:51 +0800

    Create ishanjing.lua

---
+++ ishanjing.lua
@@ -0,0 +1,9 @@
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/unn0wnn/UFCK/main/fluxusUI.lua"))()
+local window = Lib:CreateWindow("zsohe.ugc")
+
+-- ██████████████████████ Tab List ██████████████████████
+local normal = window:CreateTab("Main")
+
+normal:CreateButton("anjing", function()
+   print("kontol")
+end)
